419437c6cd5628182f51056d0e6e4889d03a5392
Lucas Passos <lucas.depassos@yahoo.com.br>
2021-01-28 14:18:57 -0300

    breakpoints

--- components/bottle.js
+++ components/bottle.js
@@ -26,6 +26,39 @@ export const BottleImg = styled.img`
     right:1%;
     overflow:hidden;
 
+    @media screen and (max-width: 1653px) {
+        width:300px;
+    }
+
+    @media screen and (max-width: 1441px) {
+        width:240px;
+        height:310px;
+    }
+
+    @media screen and (max-width: 1305px) {
+        width:210px;
+        height:330px;
+    }
+    @media screen and (max-width: 1094px) {
+        width:200px;
+        height:320px;
+    }
+    @media screen and (max-width: 888px) {
+        width:180px;
+        height:300px;
+    }
+
+    @media screen and (max-width: 788px) {
+        right:50%;
+        top:%0.2;
+        width:380px;
+        height:480px;
+        position:absolute;
+    }
+    @media screen and (max-width: 625px) {
+        width:300px;
+        height:400px;
+    }
 `
 
 
@@ -35,6 +68,26 @@ export const BottleP = styled.p`
     top:47%;
     right:2%;
     text-align:right;
+    
+    @media screen and (max-width: 2705px) {
+        top:40%;
+    }
+
+    @media screen and (max-width: 1441px) {
+        top:30%;
+        font-size:28px;
+    }
+    @media screen and (max-width: 1094px) {
+        top:30%;
+    }
+    
+    @media screen and (max-width: 788px) {
+        right:2%;
+        top:1%;
+        font-size:40px;        
+        position:absolute;
+    }
+    
     
    
 `
--- pages/about.js
+++ pages/about.js
@@ -15,51 +15,54 @@ function Page() {
     
 
     return (
-        <div className={styles.rest}>
-        <div className={styles.container}>
-            <Head>
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-            <link rel="preconnect" href="https://fonts.gstatic.com"></link>
-            <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"></link>
-            <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet"></link>
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-            </Head>
-        
-        <div className={styles.sectionHolder}>
-            <span className={styles.intro}>Allow us to introduce ourselves.
+        <div>
+<div className={styles.rest}>
+    <div className={styles.introContainer}>
+<span className={styles.intro}>Allow us to introduce ourselves.
             Prazer! <span className={styles.introItalic}>&#123;Portuguese for: it’s nice to meet you.&#125;</span> We are D.M. a new sparkling wine brand from Brazil, with our grapes grown in the Valley of the Vineyard in Southern Brazil. <span className={styles.introItalic}>An entirely new class of sparkling wine.</span> Try us and see what we mean.</span>
 
             <button className={styles.shopBtn}>SHOP</button>
-        <div className={styles.freeShippingWrap}>
-            <div className={styles.shippingMove}>
-            <p className="marquee">
-                <span>FREE SHIPPING - FREE SHIPPING - FREE SHIPPING - FREE SHIPPING - FREE SHIPPING - FREE SHIPPING - FREE SHIPPING - FREE SHIPPING - FREE SHIPPING -FREE SHIPPING - FREE SHIPPING - FREE SHIPPING - FREE SHIPPING -FREE SHIPPING - FREE SHIPPING - FREE SHIPPING - FREE SHIPPING -FREE SHIPPING - FREE SHIPPING - FREE SHIPPING -&nbsp;</span>
-                </p>
-               
-                                
-                               
-            </div>
-        </div>
-
-        <div className={styles.intro2Holder}>
-           
-            <span className={styles.intro2}>Made in the traditional champagne method with a Brazilian touch, D.M. is sparkling wine that is uniquely flexible and versatile. <span className={styles.introItalic}>You can drink</span> D.M.  <span className={styles.introItalic}>however you like, whenever you like.</span> Each sip brings the beauty, culture, and expression that is the best of old and new world winemaking. <span className={styles.introItalic}>It’s what makes us unmistakably Brazilian.</span></span>
-            
-            </div>
-        </div>
-            
-           
-
-           
-       
-        </div>
-
-
-        <Bottle />
-
         
-        <Footer>aaaaaaaaaaaaaa</Footer>
-      
+</div>
+<div className={styles.freeShippingWrap}>
+
+
+<div className={styles.shippingMove}>
+    <p className="marquee">
+        <span>FREE SHIPPING - FREE SHIPPING - FREE SHIPPING - FREE SHIPPING - FREE SHIPPING - FREE SHIPPING - FREE SHIPPING - FREE SHIPPING - FREE SHIPPING -FREE SHIPPING - FREE SHIPPING - FREE SHIPPING - FREE SHIPPING -FREE SHIPPING - FREE SHIPPING - FREE SHIPPING - FREE SHIPPING -FREE SHIPPING - FREE SHIPPING - FREE SHIPPING -&nbsp;</span>
+        </p>
+</div>
+</div>
+<div className={styles.introContainer}>
+<div className={styles.intro2Holder}>
+           
+           <span className={styles.intro2}>Made in the traditional champagne method with a Brazilian touch, D.M. is sparkling wine that is uniquely flexible and versatile. <span className={styles.introItalic}>You can drink</span> D.M.  <span className={styles.introItalic}>however you like, whenever you like.</span> Each sip brings the beauty, culture, and expression that is the best of old and new world winemaking. <span className={styles.introItalic}>It’s what makes us unmistakably Brazilian.</span></span>
+           
+           </div>
+           </div>
+
+           <Bottle />
+</div>
+<div className={styles.fixedFooter}>
+    <input className={styles.footerInput} value=" Type your email..."></input>
+    <button className={styles.footerSub}>Subscribe</button>
+
+    <p className={styles.footerP}>CC Dom Maria 2020</p>
+    <div className={styles.footerAHolder}>
+        <a href="">Facebook</a>
+        <a href="">Instagram</a>
+
+    </div>
+    <div className={styles.footerAHolder2}>
+    <a href="">Privacy</a>
+    <a href="">Shipping Policy</a>
+    <a href="">Disclaimer</a>
+    </div>
+</div>
+
+
+
+
 
         </div>
         )
